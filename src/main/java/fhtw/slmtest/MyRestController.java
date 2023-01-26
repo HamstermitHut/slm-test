729b75e4630104f578c463b4e4ec47d90981e310
Markus Frank <ic22b072@technikum-wien.at>
2023-01-26 18:22:36 +0100

    created square function in MyRestController

--- src/main/java/fhtw/slmtest/MyRestController.java
+++ src/main/java/fhtw/slmtest/MyRestController.java
@@ -1,8 +1,13 @@
 package fhtw.slmtest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
-
+    @GetMapping("/api/square/{num}")
+    public double square(@PathVariable double num){
+        return num*num;
+    }
 }
